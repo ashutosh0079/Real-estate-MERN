e2f852920aa4f06b4f3af2872bda5666a8c17fec
Ashutosh Gupta <ashu07988@gmail.com>
2024-06-19 17:10:04 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,4 +5,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      ".prisma/client/index-browser": "./node_modules/.prisma/client/index-browser.js"
+    }
+  }
 })
