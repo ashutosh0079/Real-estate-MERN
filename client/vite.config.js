e487c91cee894f65976cecd6dfe918b72b187d65
Ashutosh Gupta <ashu07988@gmail.com>
2024-06-19 13:22:43 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,20 +6,3 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
-=======
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://real-estate-mern-psi.vercel.app/',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
-});
->>>>>>> eab7d3dae1e99033a312428eae7f442cc746999c
